4b6042d27aefb7ab2a535c9269b8d513b5440138
Mohit <mohit.padhye2011@gmail.com>
2020-10-21 17:24:28 +0545

    search form filtering

--- listings/views.py
+++ listings/views.py
@@ -26,10 +26,42 @@ def listing(request,listing_id):
  
 
 def search(request):
-    context = {
+    queryset_list= Listing.objects.order_by('-list_date')
 
+    #keywords
+    if 'keywords' in request.GET:
+        keywords= request.GET['keywords']
+        if keywords:
+            queryset_list=queryset_list.filter(description__icontains=keywords)
+
+    # city
+    if 'city' in request.GET:
+        citys = request.GET['city']
+        if citys:
+             queryset_list = queryset_list.filter(city__iexact=citys)
+
+    # state
+    if 'state' in request.GET:
+        state = request.GET['state']
+        if state:
+             queryset_list = queryset_list.filter(city__iexact=state)
+
+    # bedrooms
+    if 'bedrooms' in request.GET:
+        bedrooms = request.GET['bedrooms']
+        if bedrooms:
+            queryset_list = queryset_list.filter(bedrooms__lte=bedrooms)
+
+    # price
+    if 'price' in request.GET:
+        price = request.GET['price']
+        if price:
+            queryset_list = queryset_list.filter(price__lte=price)
+
+    context = {
         'state_choices': state_choices,
         'bedroom_choices': bedroom_choices,
         'price_choices': price_choices,
+        'listings': queryset_list
     }
     return render(request,'listing/search.html',context)
