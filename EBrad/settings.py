59b0f5d709d2596bf93e6c21f90703cf9a0b80d0
Mohit <mohit.padhye2011@gmail.com>
2020-10-25 14:18:47 +0545

    update files

--- EBrad/settings.py
+++ EBrad/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ux9veiv$ca^zfge4h*2v=l_&$h9fep_n%t%dks-siqu4j7@)ba'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
